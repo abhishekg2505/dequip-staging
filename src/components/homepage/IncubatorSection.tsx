a55d3d2c44afb05dbb0b2396e99e645978c27455
abhishekg2505 <abhishekg2505@gmail.com>
2025-08-21 21:09:53 +0530

    update buttons and form

--- src/components/homepage/IncubatorSection.tsx
+++ src/components/homepage/IncubatorSection.tsx
@@ -125,7 +125,7 @@ const IncubatorSection = () => {
               </p>
             </div>
             <div ref={rightCardRef} className="mt-6 text-center w-full flex justify-center">
-              <Link href="/apply" target="_blank">
+              <Link href="/apply">
                 <Button variant="outline" className="group relative overflow-hidden">
                   <span className="text-p2 font-montserrat bg-[linear-gradient(180deg,_rgba(255,255,255,0.3)_8.85%,_#FFFFFF_100%)] bg-clip-text text-transparent">
                     Apply Now
